059db5a12f1ba66bc3b161ad51816bb88e8c4899
jamersonmychael <jamersonmychael1990@gmail.com>
2025-06-08 17:13:37 -0300

    Final Commit

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import sys
+from code.Game import Game
 
-import pygame
-
-print("Setup Start")
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-print("Setup End")
-
-print("Loop Start")
-while True:
-    #Check for all events // Checa todos os eventos
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #Close Widow // Fecha Janela
-            quit() # End pygame // Fim do pygame
-
+game = Game()
+game.run()
